3343b8f1f8d19f5a984725e902d1c0d5a844ed89
jsriveraa1 <juan.sebastian.rivera.abreo@gmail.com>
2023-09-11 20:58:36 -0500

    Update settings.py

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "3.237.236.43",  # Dirección IP de la base de datos
+        "HOST": "54.163.189.122",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
