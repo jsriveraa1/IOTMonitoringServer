51c4f5c7753ed0c41b0211938da928f0ed787fe6
jsriveraa1 <juan.sebastian.rivera.abreo@gmail.com>
2023-09-11 22:12:06 -0500

    Update settings.py

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -155,7 +155,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "204.236.193.49"
+MQTT_HOST = "34.234.100.186"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
